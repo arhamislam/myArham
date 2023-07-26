a490306f6d54467fb9c6f873f231924c1395cb99
Arham Islam <arham.m.islam@gmail.com>
2023-07-26 17:46:07 -0400

    Created basic files

---
+++ functions.php
@@ -0,0 +1,8 @@
+<!--
+    Author: Arham Islam
+    File Description: PHP functions file.
+    Date Created: July 26, 2023
+-->
+
+<?php
+?>
---
+++ index.php
@@ -0,0 +1,21 @@
+<!--
+    Author: Arham Islam
+    File Description: Login page for patients, doctors, and admins.
+    Date Created: July 26, 2023
+-->
+
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+        <link rel="stylesheet" href="styles.css">
+        <!-- <link rel="icon" href="favicon.ico"> -->
+        <title>myHealth | Login</title>
+    </head>
+    <body>
+        <script src="https://code.jquery.com/jquery-3.7.0.js" integrity="sha256-JlqSTELeR4TLqP0OG9dxM7yDPqX1ox/HfgiSLBj8+kM=" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+    </body>
+</html>
